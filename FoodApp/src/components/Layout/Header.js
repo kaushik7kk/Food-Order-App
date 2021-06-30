1d8e04d1b7210fd70f2aa7368982424e2b3bf37e
Kushagra <59172103+kaushik7kk@users.noreply.github.com>
2021-06-30 22:02:03 +0530

    Create Header.js

---
+++ FoodApp/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import styles from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+    return (
+        <Fragment>
+            <header className={styles.header} >
+                <h1>ReactMeals</h1>
+                <HeaderCartButton onClick={props.onShowCart} />
+            </header>
+            <div className={styles['main-image']} >
+                <img src={mealsImage} alt='Food Table' />
+            </div>
+        </Fragment>
+    );
+}
+
+export default Header;
